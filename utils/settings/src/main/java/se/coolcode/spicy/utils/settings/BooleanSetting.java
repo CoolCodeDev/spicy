7947391d7fbe4e9620a69b6ee1488fdbd0860249
Johnny Nilsson <johnny@coolcode.se>
2022-03-01 21:28:24 +0100

    refactor: use abstract setting

--- utils/settings/src/main/java/se/coolcode/spicy/utils/settings/BooleanSetting.java
+++ utils/settings/src/main/java/se/coolcode/spicy/utils/settings/BooleanSetting.java
@@ -1,50 +1,14 @@
 package se.coolcode.spicy.utils.settings;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.function.BiConsumer;
+public class BooleanSetting extends AbstractSetting<Boolean> {
 
-public class BooleanSetting implements Setting<Boolean> {
-
-    private String key;
-    private boolean value;
-    private Set<BiConsumer<Boolean, Boolean>> listeners;
-
-    public BooleanSetting(String key, boolean value) {
-        this.key = key;
-        this.value = value;
-        this.listeners = new HashSet<>();
+    BooleanSetting(String key, Boolean value) {
+        super(key, value, Boolean.TYPE);
     }
 
     @Override
-    public Class<Boolean> getType() {
-        return Boolean.TYPE;
-    }
-
-    @Override
-    public String getKey() {
-        return key;
-    }
-
-    @Override
-    public Boolean getValue() {
-        return value;
-    }
-
-    @Override
-    public void updateValue(String newValue) {
-        boolean updatedValue = Boolean.parseBoolean(newValue);
-        if (value != updatedValue) {
-            boolean oldValue = value;
-            value = updatedValue;
-            listeners.stream()
-            .forEach(listener -> listener.accept(oldValue, value));
-        }
-    }
-
-    @Override
-    public void addListener(BiConsumer<Boolean, Boolean> listener) {
-        this.listeners.add(listener);
+    protected Boolean parse(String newValue) {
+        return Boolean.parseBoolean(newValue);
     }
     
 }
